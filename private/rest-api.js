2ae6b457c1a9650430a03673a27396aaef417f66
Alex <alexsheen@uchicago.edu>
2021-05-05 18:06:45 -0500

    Uploading sample JSON, Example GET requests for username using user ID

--- private/rest-api.js
+++ private/rest-api.js
@@ -3,6 +3,7 @@ var fs = require('fs')
 
 const router = express.Router()
 
+/* Base Example */
 router.get('/api/example', (req, res) => {
   const obj = {
     success: true,
@@ -11,10 +12,35 @@ router.get('/api/example', (req, res) => {
   res.json(obj)
 })
 
+/* Example: Listing All Users
+ *
+ * URL: http://localhost:8000/api/listUsers
+ */
 router.get('/api/listUsers', (req, res) => {
   fs.readFile('private/users_example.json', 'utf8', (err, data) => {
     console.log(data)
     res.end(data)
+  })
+})
+
+/* Example: Query Parameters
+ *
+ * URL: http://localhost:8000/api/users/1
+ * URL: http://localhost:8000/api/users/2
+ */
+router.get('/api/users/:id', (req, res) => {
+  res.send(req.params.id)
+})
+
+/* Example: Retrieve Username using ID
+ *
+ * URL: http://localhost:8000/api/users/1/username
+ * URL: http://localhost:8000/api/users/2/username
+ */
+router.get('/api/users/:id/username', (req, res) => {
+  fs.readFile('private/users_example.json', 'utf8', (err, data) => {
+    var dataObj = JSON.parse(data);
+    res.end(dataObj.users.find(x => x.id === req.params.id).username)
   })
 })
 
