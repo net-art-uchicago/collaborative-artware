915a0a665a42f78c2d363c7b99a86b87abf4421b
Alex <alexsheen@uchicago.edu>
2021-05-05 17:37:42 -0500

    users.json template, demo GET

--- private/rest-api.js
+++ private/rest-api.js
@@ -1,4 +1,6 @@
 const express = require('express')
+var fs = require('fs')
+
 const router = express.Router()
 
 router.get('/api/example', (req, res) => {
@@ -7,6 +9,13 @@ router.get('/api/example', (req, res) => {
     message: 'this is an example'
   }
   res.json(obj)
+})
+
+router.get('/api/listUsers', (req, res) => {
+  fs.readFile('private/users_example.json', 'utf8', (err, data) => {
+    console.log(data)
+    res.end(data)
+  })
 })
 
 module.exports = router
