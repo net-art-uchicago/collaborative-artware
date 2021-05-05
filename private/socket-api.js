d044fbd4e009fe855c5d1e010277b6634fce958b
Marco Anaya <manaya@uchicago.edu>
2021-05-05 15:27:30 -0500

    adding code for a basic whiteboard to investigate what is needed for documentation

--- private/socket-api.js
+++ private/socket-api.js
@@ -1,3 +1,22 @@
+const allUsers = {}
+
 module.exports = (socket, io) => {
   console.log(`user ${socket.id} has connected!`)
+
+  socket.on('newUser', (user) => {
+    // send new user to all exisitng users
+    socket.broadcast.emit('newUser', user)
+    // send all users back to the new user
+    io.to(user.socketId).emit('getExistingUsers', allUsers)
+    // add new user to object that olds all users
+    allUsers[user.id] = user
+  })
+
+  socket.on('draw', (data) => {
+    socket.broadcast.emit('draw', data)
+  })
+
+  socket.on('disconnect', () => {
+    console.log('Client has disconnected')
+  })
 }
