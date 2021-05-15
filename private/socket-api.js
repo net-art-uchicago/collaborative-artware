a88f48ab0eb51950e463d210a51ea0d4cd873827
Marco Anaya <manaya@uchicago.edu>
2021-05-15 14:20:30 -0500

    simplifying code based on last class/documentation

--- private/socket-api.js
+++ private/socket-api.js
@@ -1,11 +1,11 @@
-const allUsers = []
-
 module.exports = (socket, io) => {
   console.log(`user ${socket.id} has connected!`)
 
   socket.on('newConnection', (user) => {
     // send all users back to the new user
-    socket.to(io.sockets.sockets.keys().next().value).emit('sendExistingInfo', socket.id)
+    const clientIDs = [...io.sockets.sockets.keys()]
+    const senderClientID = clientIDs[Math.floor(Math.random() * clientIDs.length)]
+    socket.to(senderClientID).emit('sendExistingInfo', socket.id)
 
     // send new user to all exisitng users
     socket.broadcast.emit('newConnection', user)
@@ -13,19 +13,17 @@ module.exports = (socket, io) => {
     // add new user to object that olds all users
     socket.userId = user.id
   })
-  
-  socket.on('sendExistingInfo', ({ socketId, ...data }) => {
+
+  socket.on('getExistingInfo', ({ socketId, ...data }) => {
     socket.to(socketId).emit('getExistingInfo', data)
   })
 
-
-  socket.on('draw', (data) => {
-    socket.broadcast.emit('draw', data)
+  socket.on('mouse', (data) => {
+    socket.broadcast.emit('mouse', data)
   })
 
   socket.on('disconnect', () => {
     socket.broadcast.emit('newDisconnect', socket.userId)
-    delete allUsers[socket.userId]
     console.log('Client has disconnected')
   })
 }
