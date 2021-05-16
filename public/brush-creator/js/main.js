e200ccb4bf84f9402f433fd92fc184ee17bdbaef
tovarm-uchi <mel@DESKTOP-E293HAQ.localdomain>
2021-05-16 12:08:50 -0500

    fixing standardjs but now other js files like p5 not being recongnized

--- public/brush-creator/js/main.js
+++ public/brush-creator/js/main.js
@@ -1,13 +1,11 @@
 /* global */
-var stamp = null; 
-var stampIMG = null;
-var lines = [];
-var brushes = [];
-var defaultBrushes = [
+let stamp = null
+let lines = []
+const defaultBrushes = [
   {
     name: 'none',
-    draw: function(p){
-      for(var line of lines){
+    draw: function (p) {
+      for (const line of lines) {
         line.show()
       }
     }
@@ -20,14 +18,14 @@ var defaultBrushes = [
         p.ellipse(p.mouseX, p.mouseY, radius, radius)
       }
     }
-  }, 
-  {  
+  },
+  {
     name: 'wordbrush',
     draw: function (p) {
       if (p.mouseIsPressed) {
         const radius = p.sin(p.frameCount * 0.1) * 40
-        let words = ["Collaborative", "Art" , "Ware","2021" ];
-        let word = p.random(words);
+        const words = ['Collaborative', 'Art', 'Ware', '2021']
+        const word = p.random(words)
         p.text(word, p.mouseX, p.mouseY, radius, radius)
       }
     }
@@ -41,7 +39,7 @@ var defaultBrushes = [
       }
     }
   },
-  {  
+  {
     name: 'linework',
     draw: function (p) {
       if (p.mouseIsPressed) {
@@ -49,109 +47,97 @@ var defaultBrushes = [
         p.line(p.mouseX, p.mouseY, p.mouseX + radius, p.mouseY + radius)
       }
     }
-}
+  }
 ]
-var currBrush = defaultBrushes[0];
+let currBrush = defaultBrushes[0]
 
-function updateBrush(bindex){
-  currBrush = defaultBrushes[bindex];
-  stamp = null;
-}
-
-var create = function(p){
+const create = function (p) {
   /* local global */
-  p.pad;
-  p.clearBut;
-  p.saveBut;
+  let pad, clearBut, saveBut
 
   p.setup = () => {
-    p.pad = p.select("#create")
-    p.clearBut = p.select("#clear")
-    p.saveBut = p.select("#save")
-    p.cnvs = p.createCanvas(p.windowHeight / 2 , p.windowHeight / 2)
-    p.cnvs.parent(p.pad)
+    pad = p.select('#create')
+    clearBut = p.select('#clear')
+    saveBut = p.select('#save')
+    p.cnvs = p.createCanvas(p.windowHeight / 2, p.windowHeight / 2)
+    p.cnvs.parent(pad)
   }
 
-  p.windowResized = () => { 
+  p.windowResized = () => {
     p.resizeCanvas(p.windowHeight / 2, p.windowHeight / 2)
   }
 
   p.draw = () => {
-    p.clearBut.mousePressed(() => {
+    clearBut.mousePressed(() => {
       p.clear()
       lines = []
     })
-    
-    p.saveBut.mousePressed(() => {
-      var brushName = p.select("#bname").value()
-      var brushStamp = p.get().canvas.toDataURL() // base64 encoding
-      var newBrushButton = p.createImg(brushStamp).addClass("brushImg").id(brushName).parent(p.select(".brushesContainer"))
+
+    saveBut.mousePressed(() => {
+      const brushName = p.select('#bname').value()
+      const brushStamp = p.get().canvas.toDataURL() // base64 encoding
+      const newBrushButton = p.createImg(brushStamp).addClass('brushImg').id(brushName).parent(p.select('.brushesContainer'))
       defaultBrushes.push({
         name: brushName,
-        draw: function(p) {
+        draw: function (p) {
           p.loadImage(brushStamp, brushImg => {
-            if(p.mouseIsPressed){
+            if (p.mouseIsPressed) {
               p.image(brushImg, p.mouseX, p.mouseY, 50, 50)
             }
           })
         }
       })
 
-      newBrushButton.mousePressed(()=>{
-        var name = newBrushButton.id()
-        for(var b of defaultBrushes){
-          if(b.name == name){
-            currBrush = b;
-            return;
+      newBrushButton.mousePressed(() => {
+        const name = newBrushButton.id()
+        for (const b of defaultBrushes) {
+          if (b.name === name) {
+            currBrush = b
+            return
           }
         }
       })
-  
-    })     
-    
+    })
+
     if (p.mouseIsPressed) {
-      var line = new MyLine(p)
+      const line = new MyLine(p)
       lines.push(line)
     }
-    
+
     // change / adjust code here to take in pre-define or differing shape?
-    if(p.mouseIsPressed){
+    if (p.mouseIsPressed) {
       currBrush.draw(p)
       stamp = p.get().canvas.toDataURL();
     }
   }
 }
 
-var createPad = new p5(create)
-
-
-var test = function(p){
+const test = function (p) {
   /* local global */
-  p.pad;
-  p.clearBut;
+  let pad
+  let clearBut
 
   p.setup = () => {
-    p.pad = p.select("#test")
-    p.clearBut = p.select("#clear")
+    p.pad = p.select('#test')
+    p.clearBut = p.select('#clear')
     p.cnvs = p.createCanvas(p.windowHeight / 2, p.windowHeight / 2)
-    p.cnvs.parent(p.pad)
+    p.cnvs.parent(pad)
     p.background(225)
   }
 
-  p.windowResized = () => { 
+  p.windowResized = () => {
     p.resizeCanvas(p.windowHeight / 2, p.windowHeight / 2)
     p.background(225)
   }
- 
-  p.draw = () => {
 
-    p.clearBut.mousePressed(() => {
+  p.draw = () => {
+    clearBut.mousePressed(() => {
       p.background(225)
     })
-    
-    if(stamp){
+
+    if (stamp) {
       p.loadImage(stamp, stampIMG => {
-        if(p.mouseIsPressed){
+        if (p.mouseIsPressed) {
           p.image(stampIMG, p.mouseX, p.mouseY, 50, 50)
         }
       })
@@ -159,4 +145,5 @@ var test = function(p){
   }
 }
 
-var testPad = new p5(test)
+p5(create)
+p5(test)
