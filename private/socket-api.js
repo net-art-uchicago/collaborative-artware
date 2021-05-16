cbd9247ac0eb36a026692455b688e17311dcd0d3
Marco Anaya <manaya@uchicago.edu>
2021-05-16 00:28:45 -0500

    fixing send/get existing user endpoint

--- private/socket-api.js
+++ private/socket-api.js
@@ -1,21 +1,26 @@
+const userToSocketId = new Map()
+
 module.exports = (socket, io) => {
   console.log(`user ${socket.id} has connected!`)
 
   socket.on('newConnection', (user) => {
     // send all users back to the new user
-    const clientIDs = [...io.sockets.sockets.keys()]
-    const senderClientID = clientIDs[Math.floor(Math.random() * clientIDs.length)]
-    socket.to(senderClientID).emit('sendExistingInfo', socket.id)
+    const clientIds = [...userToSocketId.entries()]
+      .filter(([uid, sid]) => sid !== socket.id && uid !== user.id)
+      .map(([, sid]) => sid)
+    const senderClientId = clientIds[0]// Math.floor(Math.random() * clientIds.length)]
+    socket.to(senderClientId).emit('sendExistingInfo', { socketId: socket.id })
 
-    // send new user to all exisitng users
+    // send new user to all existing users
     socket.broadcast.emit('newConnection', user)
 
     // add new user to object that olds all users
+    userToSocketId.set(user.id, socket.id)
     socket.userId = user.id
   })
 
-  socket.on('getExistingInfo', ({ socketId, ...data }) => {
-    socket.to(socketId).emit('getExistingInfo', data)
+  socket.on('getExistingInfo', (data) => {
+    socket.to(data.socketId).emit('getExistingInfo', data)
   })
 
   socket.on('mouse', (data) => {
@@ -23,7 +28,11 @@ module.exports = (socket, io) => {
   })
 
   socket.on('disconnect', () => {
+    // const potentialIds = [...userToSocketId.entries()].find(([uid, sid]) => sid === socket.id)
+    // if (potentialIds !== undefined) socket.broadcast.emit('newDisconnect', potentialIds[0])
+    userToSocketId.delete(socket.userId)
     socket.broadcast.emit('newDisconnect', socket.userId)
-    console.log('Client has disconnected')
+
+    console.log(`Client ${socket.id} has disconnected`)
   })
 }
