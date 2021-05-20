cd7107c86247360de89a4f8a3e92ef43d3fd428c
Nicole <navila@uchicago.edu>
2021-05-20 12:46:55 -0500

    update

--- public/whiteboard/js/main.js
+++ public/whiteboard/js/main.js
@@ -1,6 +1,7 @@
 /* global */
 let img
 let dumBrush
+
 function dummyBrush(){
   let img = createImage(66, 66)
   img.loadPixels()
@@ -9,12 +10,13 @@ function dummyBrush(){
 
       img.set(i, j, color(0, 90, 102,( abs(img.width/ 2 - i)) * 2))
       //img.set(i+1, j, color(0, 90, 102,( abs(img.width/ 2 - i)) * 2))
-
     }
   }
   img.updatePixels()
   return img
 }
+
+
 function preload(){
   // Currently have a dummy image but we will be recieving images from the backend
   img = loadImage('https://media.istockphoto.com/vectors/cute-cartoon-cockatiel-couple-vector-id1160619330?b=1&k=6&m=1160619330&s=612x612&w=0&h=V6NM2xCwQ16AyIEK0prZKqm6NNjMfgZN7VUyL6HaELs=')
@@ -27,10 +29,11 @@ function setup () {
 function windowResized () {
   resizeCanvas(window.innerWidth, window.innerHeight)
 }
-
+// changed 
 function draw () {
   if (mouseIsPressed) {
     //ellipse(mouseX, mouseY, 50)
+    // use imageMode(CENTER) to center the pic when you click
       image(dumBrush, mouseX, mouseY, 50,50)
   }
 }
