2441d20fa7aa2f376f70445e9e2a1c3d90c560a9
Marco Anaya <manaya@uchicago.edu>
2021-05-23 00:47:29 -0500

    class for ones own user

---
+++ public/whiteboard/js/OwnUser.js
@@ -0,0 +1,22 @@
+/* global User */
+class OwnUser extends User {
+  // on connect, send your user to server
+  broadcast () {
+    this.socket.on('connect', () => {
+      this.socket.emit('broadcastWhiteboardUser', this.toObject())
+    })
+  }
+
+  draw (x, y) {
+    super.draw(x, y)
+    this.socket.emit('broadcastWhiteboardMouse', {
+      id: this.id,
+      pos: { x, y },
+      brush: this.brushPath,
+      size: this.brush.size,
+      color: this.brush.color.toString()
+    })
+  }
+}
+
+window.OwnUser = OwnUser
